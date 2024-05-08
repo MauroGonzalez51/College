fdde94e8e22a461bfaa892d8dc3148f5c8e596ee
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2024-05-08 19:58:26 +0000

    Implementation of the methods push, pop shift is still in progress

--- basic-double-linked-list/include/classes/double_linked_list.h
+++ basic-double-linked-list/include/classes/double_linked_list.h
@@ -1,6 +1,7 @@
 #ifndef DOUBLE_LINKED_LIST_H
 #define DOUBLE_LINKED_LIST_H
 #include <iostream>
+#include <stdexcept>
 #include "../node.h"
 
 class DoubleLinkedList {
@@ -11,9 +12,42 @@ class DoubleLinkedList {
     public:
         DoubleLinkedList(): head(nullptr), tail(nullptr), currentSize(0) {}
         DoubleLinkedList(const DoubleLinkedList *);
-        int push(short);
+
         Node *getHead() const;
         Node *getTail() const;
+
+        /**
+        * Adds a new element to the end of the double linked list.
+        *
+        * @param {number} value - The value to be added to the list.
+        * @returns {number} The new size of the list after adding the element.
+        */
+        int push(short);
+        
+        /**
+         * Adds a new element at the beggining of the double linked list.
+         * 
+         * @param {number} value - The value to be added to the list.
+         * @returns {number} The new size of the list after adding the element. 
+        */
+        int unshift(short);
+
+        /**
+         * Removes the last node of the list, and returns it's
+         * value
+         * 
+         * @returns {number} - Value of the last element
+        */
+       short pop();
+
+       /**
+        * Removes the first element of the list, and returns it's
+        * value
+        * 
+        * @returns {number} - Value of the first element
+       */
+
+        short shift();
 };
 
 #endif
--- basic-double-linked-list/src/double_linked_list.cpp
+++ basic-double-linked-list/src/double_linked_list.cpp
@@ -28,13 +28,50 @@ int DoubleLinkedList::push(short newValue) {
     if (!this -> head) {
         this -> head = node;
         this -> tail = node;
-        this -> currentSize++;
-        return this -> currentSize;
+        return ++this -> currentSize;
     }
 
     this -> tail -> nextElement = node;
     node -> previousElement = this -> tail;
     this -> tail = node;
     this -> currentSize++;
-    return this -> currentSize;
+    return ++this -> currentSize;
+}
+
+int DoubleLinkedList::unshift(short newValue) {
+    Node *node = new Node(newValue);
+
+    if (!node) return 0;
+
+    if (!this -> head) {
+        delete node;
+        return this -> push(newValue);
+    }
+
+    this -> head -> previousElement = node;
+    node -> nextElement = this -> head;
+    this -> head = node;
+    return ++this -> currentSize;
+}
+
+short DoubleLinkedList::pop() {
+    if (!this -> tail)
+        throw std::out_of_range("Cannot pop from an empty list");
+
+    Node *node = this -> head;
+    short value = node -> value;
+
+    this -> tail -> previousElement -> nextElement = nullptr;
+    
+    delete node;
+}
+
+short DoubleLinkedList::shift() {
+    if (!this -> head)
+        throw std::out_of_range("Cannot shift from an empty list");
+
+    short value = this -> head -> value;
+
+
+
 }
