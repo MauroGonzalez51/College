23a0c5fbb1883b266fed6187fe2f78a0a8d3517e
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2024-05-14 19:04:14 -0500

    Testing the Hexadecimal class

--- basic-double-linked-list/main.cpp
+++ basic-double-linked-list/main.cpp
@@ -2,6 +2,7 @@
 #include "classes/decimal.h"
 #include "classes/binary.h"
 #include "classes/binary_linked.h"
+#include "classes/hexadecimal.h"
 
 int main(int argc, char **argv) {
     if (argc < 3) {
@@ -9,12 +10,10 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    BinaryLinked *b1 = new BinaryLinked(std::stoi(argv[1]));
-    BinaryLinked *b2 = new BinaryLinked(std::stoi(argv[2]));
+    Hexadecimal *h1 = new Hexadecimal(std::stoi(argv[2]));
+    Hexadecimal *h2 = new Hexadecimal(std::stoi(argv[3]));
 
-    BinaryLinked *result = (*b1) + (*b2);  
-    
-    std::cout << *result << std::endl;
+    std::cout << *h1 + *h2 << std::endl;
 
     return EXIT_SUCCESS;
 }
